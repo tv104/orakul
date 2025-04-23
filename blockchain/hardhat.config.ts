bc5202fc152dc069a34c7e8c33c0a223e7a5ae84
tv <tv104@users.noreply.github.com>
2025-04-23 16:39:35 +0200

    add blockchain tooling with hardhat

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  paths: {
+    artifacts: '../src/artifacts',
+    sources: "./contracts",
+    cache: "./cache",
+    tests: "./test"
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337
+    }
+  },
+};
+
+export default config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/blockchain/test/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "off",
+    }
+  }
 ];
 
 export default eslintConfig;
