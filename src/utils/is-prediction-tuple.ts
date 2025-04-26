6714ba4adac9f11e73dc27e62571b65439059b46
tv <tv104@users.noreply.github.com>
2025-04-26 10:33:51 +0200

    fix tupe result type

--- src/utils/is-prediction-tuple.ts
+++ src/utils/is-prediction-tuple.ts
@@ -1,9 +1,9 @@
-type PredictionTuple = [boolean, bigint, string, string];
+type PredictionTuple = [boolean, number, string, string];
 
 export function isPredictionTuple(data: unknown): data is PredictionTuple {
   return Array.isArray(data) && data.length === 4 && 
     typeof data[0] === 'boolean' && 
-    typeof data[1] === 'bigint' && 
+    typeof data[1] === 'number' && // viem automatically converts small integers to numbers
     typeof data[2] === 'string' && 
     typeof data[3] === 'string';
 }
