8522cdb9bc71fbc5a11e93a5e7378bd3721edbcc
tv <tv104@users.noreply.github.com>
2025-04-29 00:27:38 +0200

    fix slow hardhat tests

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const isTest = process.env.NODE_ENV === "test";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   paths: {
@@ -13,8 +15,8 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 31337,
       mining: {
-        auto: false,
-        interval: 1000
+        auto: !isTest,
+        ...(isTest ? {} : { interval: 1000 })
       }
     },
     localhost: {
