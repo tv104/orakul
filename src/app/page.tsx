a878287b0f31dbcdc6fea5f6e20b638cd781adc3
tv <tv104@users.noreply.github.com>
2025-04-30 20:07:40 +0200

    update images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { ConnectWallet, Orakul, ParallaxBackground } from "@/components";
-import room from "@/images/room.png";
-import hands from "@/images/hands.png";
+import room from "@/images/room.jpg";
+import handsAndBall from "@/images/hands-and-ball.png";
 
 export default function Home() {
   return (
     <>
-      <ParallaxBackground backgroundImage={room} foregroundImage={hands} />
+      <ParallaxBackground
+        backgroundImage={room}
+        foregroundImage={handsAndBall}
+      />
       <main className="relative min-h-screen p-8 flex">
         <div className="max-w-6xl mx-auto flex flex-col flex-1">
           <div className="flex justify-end mb-6">
